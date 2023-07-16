ed925851bc93f182a32656b75a7bce3fa6dbabb9
Siby C R <sibycr18@gmail.com>
2023-07-16 22:10:20 +0530

    fixed key press zoom not working issue

--- app.js
+++ app.js
@@ -18,94 +18,152 @@ let tom2KeyIsPressed = false;
 
 // Function to play a drum sound and trigger animation
 function playDrum(padId, sound) {
-    console.log(padId);
-  if (!sound.paused) {
-    sound.currentTime = 0;
-  } else {
-    sound.play();
-  }
-  document.getElementById(padId).classList.add('active');
-  setTimeout(function() {
-    document.getElementById(padId).classList.remove('active');
-  }, 200);
+    if (!sound.paused) {
+        sound.currentTime = 0;
+    } else {
+        sound.play();
+    }
 }
 
 // Drum pad click event listeners
 document.getElementById('kick-pad').addEventListener('click', function() {
-  playDrum('kick-pad', kickSound);
+    playDrum('kick-pad', kickSound);
+    document.getElementById('kick-pad').style.transform = 'scale(1.05)';
+    setTimeout(function() {
+        document.getElementById('kick-pad').style.transform = 'scale(1)';
+    }, 140);
 });
 
 document.getElementById('closedhat-pad').addEventListener('click', function() {
-  playDrum('closedhat-pad', closedHatSound);
+    playDrum('closedhat-pad', closedHatSound);
+    document.getElementById('closedhat-pad').style.transform = 'scale(1.05)';
+    setTimeout(function() {
+        document.getElementById('closedhat-pad').style.transform = 'scale(1)';
+    }, 140);
 });
 
 document.getElementById('openhat-pad').addEventListener('click', function() {
-  playDrum('openhat-pad', openHatSound);
+    playDrum('openhat-pad', openHatSound);
+    document.getElementById('openhat-pad').style.transform = 'scale(1.05)';
+    setTimeout(function() {
+        document.getElementById('openhat-pad').style.transform = 'scale(1)';
+    }, 140);
 });
 
 document.getElementById('snare-pad').addEventListener('click', function() {
-  playDrum('snare-pad', snareSound);
+    playDrum('snare-pad', snareSound);
+    document.getElementById('snare-pad').style.transform = 'scale(1.05)';
+    setTimeout(function() {
+        document.getElementById('snare-pad').style.transform = 'scale(1)';
+    }, 140);
 });
 
 document.getElementById('cymbal-pad').addEventListener('click', function() {
-  playDrum('cymbal-pad', cymbalSound);
+    playDrum('cymbal-pad', cymbalSound);
+    document.getElementById('cymbal-pad').style.transform = 'scale(1.05)';
+    setTimeout(function() {
+        document.getElementById('cymbal-pad').style.transform = 'scale(1)';
+    }, 140);
 });
 
 document.getElementById('tom1-pad').addEventListener('click', function() {
-  playDrum('tom1-pad', tom1Sound);
+    playDrum('tom1-pad', tom1Sound);
+    document.getElementById('tom1-pad').style.transform = 'scale(1.05)';
+    setTimeout(function() {
+        document.getElementById('tom1-pad').style.transform = 'scale(1)';
+    }, 140);
 });
 
 document.getElementById('tom2-pad').addEventListener('click', function() {
-  playDrum('tom2-pad', tom2Sound);
+    playDrum('tom2-pad', tom2Sound);
+    document.getElementById('tom2-pad').style.transform = 'scale(1.05)';
+    setTimeout(function() {
+        document.getElementById('tom2-pad').style.transform = 'scale(1)';
+    }, 140);
 });
 
 // Keyboard event listeners
 document.addEventListener('keydown', function(event) {
-  if (event.key === 'a' && !kickKeyIsPressed) {
-    kickKeyIsPressed = true;
-    playDrum('kick-pad', kickSound);
-  } else if (event.key === 'q' && !closedHatKeyIsPressed) {
-    closedHatKeyIsPressed = true;
-    playDrum('closedhat-pad', closedHatSound);
-  } else if (event.key === 'w' && !openHatKeyIsPressed) {
-    openHatKeyIsPressed = true;
-    playDrum('openhat-pad', openHatSound);
-  } else if (event.key === 's' && !snareKeyIsPressed) {
-    snareKeyIsPressed = true;
-    playDrum('snare-pad', snareSound);
-  } else if (event.key === 'e' && !cymbalKeyIsPressed) {
-    cymbalKeyIsPressed = true;
-    playDrum('cymbal-pad', cymbalSound);
-  } else if (event.key === 'd' && !tom1KeyIsPressed) {
-    tom1KeyIsPressed = true;
-    playDrum('tom1-pad', tom1Sound);
-  } else if (event.key === 'f' && !tom2KeyIsPressed) {
-    tom2KeyIsPressed = true;
-    playDrum('tom2-pad', tom2Sound);
-  }
+    if (event.key === 'a' && !kickKeyIsPressed) {
+        kickKeyIsPressed = true;
+        playDrum('kick-pad', kickSound);
+        document.getElementById('kick-pad').style.transform = 'scale(1.05)';
+        // document.getElementById('kick-pad').style.boxShadow = '0 0 5px rgba(0, 0, 0, 0.3)';
+        setTimeout(function() {
+            document.getElementById('kick-pad').style.transform = 'scale(1)';
+        }, 140);
+
+    } else if (event.key === 'q' && !closedHatKeyIsPressed) {
+        closedHatKeyIsPressed = true;
+        playDrum('closedhat-pad', closedHatSound);
+        document.getElementById('closedhat-pad').style.transform = 'scale(1.05)';
+        setTimeout(function() {
+            document.getElementById('closedhat-pad').style.transform = 'scale(1)';
+        }, 140);
+
+    } else if (event.key === 'w' && !openHatKeyIsPressed) {
+        openHatKeyIsPressed = true;
+        playDrum('openhat-pad', openHatSound);
+        document.getElementById('openhat-pad').style.transform = 'scale(1.05)';
+        setTimeout(function() {
+            document.getElementById('openhat-pad').style.transform = 'scale(1)';
+        }, 140);
+
+    } else if (event.key === 's' && !snareKeyIsPressed) {
+        snareKeyIsPressed = true;
+        playDrum('snare-pad', snareSound);
+        document.getElementById('snare-pad').style.transform = 'scale(1.05)';
+        setTimeout(function() {
+            document.getElementById('snare-pad').style.transform = 'scale(1)';
+        }, 140);
+
+    } else if (event.key === 'e' && !cymbalKeyIsPressed) {
+        cymbalKeyIsPressed = true;
+        playDrum('cymbal-pad', cymbalSound);
+        document.getElementById('cymbal-pad').style.transform = 'scale(1.05)';
+        setTimeout(function() {
+            document.getElementById('cymbal-pad').style.transform = 'scale(1)';
+        }, 140);
+
+    } else if (event.key === 'd' && !tom1KeyIsPressed) {
+        tom1KeyIsPressed = true;
+        playDrum('tom1-pad', tom1Sound);
+        document.getElementById('tom1-pad').style.transform = 'scale(1.05)';
+        setTimeout(function() {
+            document.getElementById('tom1-pad').style.transform = 'scale(1)';
+        }, 140);
+
+    } else if (event.key === 'f' && !tom2KeyIsPressed) {
+        tom2KeyIsPressed = true;
+        playDrum('tom2-pad', tom2Sound);
+        document.getElementById('tom2-pad').style.transform = 'scale(1.05)';
+        setTimeout(function() {
+            document.getElementById('tom2-pad').style.transform = 'scale(1)';
+        }, 140);
+    }
 });
 
 document.addEventListener('keyup', function(event) {
     if (event.key === 'a') {
-    kickKeyIsPressed = false;
-    document.getElementById('kick-pad').classList.remove('active');
+        kickKeyIsPressed = false;
+        document.getElementById('kick-pad').classList.remove('active');
     } else if (event.key === 'q') {
-    closedHatKeyIsPressed = false;
-    document.getElementById('closedhat-pad').classList.remove('active');
+        closedHatKeyIsPressed = false;
+        document.getElementById('closedhat-pad').classList.remove('active');
     } else if (event.key === 'w') {
-    openHatKeyIsPressed = false;
-    document.getElementById('openhat-pad').classList.remove('active');
+        openHatKeyIsPressed = false;
+        document.getElementById('openhat-pad').classList.remove('active');
     } else if (event.key === 's') {
-    snareKeyIsPressed = false;
-    document.getElementById('snare-pad').classList.remove('active');
+        snareKeyIsPressed = false;
+        document.getElementById('snare-pad').classList.remove('active');
     } else if (event.key === 'e') {
-    cymbalKeyIsPressed = false;
-    document.getElementById('cymbal-pad').classList.remove('active');
+        cymbalKeyIsPressed = false;
+        document.getElementById('cymbal-pad').classList.remove('active');
     } else if (event.key === 'd') {
-    tom1KeyIsPressed = false;
-    document.getElementById('tom1-pad').classList.remove('active');
+        tom1KeyIsPressed = false;
+        document.getElementById('tom1-pad').classList.remove('active');
     } else if (event.key === 'f') {
-    tom2KeyIsPressed = false;
-    document.getElementById('tom2-pad').classList.remove('active');
+        tom2KeyIsPressed = false;
+        document.getElementById('tom2-pad').classList.remove('active');
     }
 });
