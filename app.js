1e6a8c60de662d7febcf8de359463948318ea3f8
Siby C R <sibycr18@gmail.com>
2023-07-16 14:40:07 +0530

    implemented design and function

---
+++ app.js
@@ -0,0 +1,103 @@
+// Drum sound objects
+const kickSound = new Audio('audio/kick.wav');
+const closedHatSound = new Audio('audio/closedhat.wav');
+const openHatSound = new Audio('audio/openhat.wav');
+const snareSound = new Audio('audio/snare.wav');
+const cymbalSound = new Audio('audio/cymbal.wav');
+const tom1Sound = new Audio('audio/tom1.wav');
+const tom2Sound = new Audio('audio/tom2.wav');
+
+// Define variables to track the key state and animation state
+let kickKeyIsPressed = false;
+let closedHatKeyIsPressed = false;
+let openHatKeyIsPressed = false;
+let snareKeyIsPressed = false;
+let cymbalKeyIsPressed = false;
+let tom1KeyIsPressed = false;
+let tom2KeyIsPressed = false;
+
+// Function to play a drum sound and trigger animation
+function playDrum(padId, sound) {
+  if (!sound.paused) {
+    sound.currentTime = 0;
+  } else {
+    sound.play();
+  }
+  document.getElementById(padId).classList.add('active');
+  setTimeout(function() {
+    document.getElementById(padId).classList.remove('active');
+  }, 200);
+}
+
+// Drum pad click event listeners
+document.getElementById('kick-pad').addEventListener('click', function() {
+  playDrum('kick-pad', kickSound);
+});
+
+document.getElementById('closedhat-pad').addEventListener('click', function() {
+  playDrum('closedhat-pad', closedHatSound);
+});
+
+document.getElementById('openhat-pad').addEventListener('click', function() {
+  playDrum('openhat-pad', openHatSound);
+});
+
+document.getElementById('snare-pad').addEventListener('click', function() {
+  playDrum('snare-pad', snareSound);
+});
+
+document.getElementById('cymbal-pad').addEventListener('click', function() {
+  playDrum('cymbal-pad', cymbalSound);
+});
+
+document.getElementById('tom1-pad').addEventListener('click', function() {
+  playDrum('tom1-pad', tom1Sound);
+});
+
+document.getElementById('tom2-pad').addEventListener('click', function() {
+  playDrum('tom2-pad', tom2Sound);
+});
+
+// Keyboard event listeners
+document.addEventListener('keydown', function(event) {
+  if (event.key === 'a' && !kickKeyIsPressed) {
+    kickKeyIsPressed = true;
+    playDrum('kick-pad', kickSound);
+  } else if (event.key === 'q' && !closedHatKeyIsPressed) {
+    closedHatKeyIsPressed = true;
+    playDrum('closedhat-pad', closedHatSound);
+  } else if (event.key === 'w' && !openHatKeyIsPressed) {
+    openHatKeyIsPressed = true;
+    playDrum('openhat-pad', openHatSound);
+  } else if (event.key === 's' && !snareKeyIsPressed) {
+    snareKeyIsPressed = true;
+    playDrum('snare-pad', snareSound);
+  } else if (event.key === 'e' && !cymbalKeyIsPressed) {
+    cymbalKeyIsPressed = true;
+    playDrum('cymbal-pad', cymbalSound);
+  } else if (event.key === 'd' && !tom1KeyIsPressed) {
+    tom1KeyIsPressed = true;
+    playDrum('tom1-pad', tom1Sound);
+  } else if (event.key === 'f' && !tom2KeyIsPressed) {
+    tom2KeyIsPressed = true;
+    playDrum('tom2-pad', tom2Sound);
+  }
+});
+
+document.addEventListener('keyup', function(event) {
+  if (event.key === 'a') {
+    kickKeyIsPressed = false;
+  } else if (event.key === 'q') {
+    closedHatKeyIsPressed = false;
+  } else if (event.key === 'w') {
+    openHatKeyIsPressed = false;
+  } else if (event.key === 's') {
+    snareKeyIsPressed = false;
+  } else if (event.key === 'e') {
+    cymbalKeyIsPressed = false;
+  } else if (event.key === 'd') {
+    tom1KeyIsPressed = false;
+  } else if (event.key === 'f') {
+    tom2KeyIsPressed = false;
+  }
+});
